2e8f2f4f570c1a3aef207811808a4798f0cfa93e
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-01-26 15:29:18 -0500

    commits to decimal now

--- NumberSystemConverter/main.swift
+++ NumberSystemConverter/main.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-//INPUT
 print("What number system do you want to convert from?")
 print("B: Binary")
 print("O: Octal")
@@ -15,6 +14,21 @@ print("D: Decimal")
 print("H: Hexadecimal")
 print("Enter your choice (B/O/D/H): ", terminator: "") //keep following output on the same line
 let from = readLine()!
+
+var startBase = 0.0
+
+switch from {
+case "B":
+    var startBase = 2.0
+case "O":
+    var startBase = 8.0
+case "D":
+    var startBase = 10.0
+case "H":
+    var startBase = 16.0
+default:
+    print("Please enter a valid number system")
+}
 
 print("What number system do you want to convert to?")
 print("B: Binary")
@@ -25,9 +39,29 @@ print("Enter your choice (B/O/D/H): ", terminator: "") //keep following output o
 let to = readLine()!
 
 print("What number do you want to convert?")
+let numberToConvert = readLine()!
 
-//PROCESS
+func getDecimalValue (of value: String, base: Double) -> Double {
+    var exponent = 0.0
+    var decimalEquivalent = 0.0
+    for character in value.reversed() {
+        if let digit = Double(String(character)){
+            decimalEquivalent += digit * pow(base, exponent)
+        } else {
+            let nextCharacter = String(character)
+            for scalar in nextCharacter.unicodeScalars {
+                decimalEquivalent += Double(scalar.value - 55) * pow(base, exponent)
+            }
+        }
+        exponent += 1
+    }
+    return (decimalEquivalent)
+    
+}
 
+let decimalValue=getDecimalValue(of: numberToConvert, base: startBase)
 
-//OUTPUT
-
+switch to {
+case:
+    
+}
